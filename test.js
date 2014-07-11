3bc018f50cef5f27d3623a8e02e9fb1359f2e175
Kyle Robinson Young <kyle@dontkry.com>
2014-07-11 11:48:46 -0700

    Add tests for node

---
+++ test.js
@@ -0,0 +1,22 @@
+var test = require('tape');
+var Convert = require('./lib/convert');
+var path = require('path');
+
+test('converts image to hash', function(t) {
+  t.plan(1);
+  var c = new Convert();
+  c.readImage(path.resolve('rabbit.png'), function(err, hash) {
+    t.equal(hash, 'A/bfhkSfdihfShaefShahfShahhYfYfYfSfSfSfYhYhYhahjSdechjYhYhYhadfQUhchfYhYhSfYdQYhYhaefQYhYhYhYhSjcchQYhYhYhYhSfSfWehSfUhShecheQYhYhYhYhachYhYhafhYhahfShXdfhShcihYaVhfYmfbihhQYhYhYhaddQShahfYhYhYhShYfYfYfafhQUhchfYhYhYhShechdUhUhcheUhUhcheUhUhcheUhUhcheUhUhWehUhUhcfeUhUhcfeUhUhcfeUhUhcfeUhUhehehUhUhcheUhUhcheUhUhcheUhUhWehUhUhcfeUhUhcfeUhUhcfeUhUhcfeUhUhWffUhWheQYhYhYhYhachQYiYhYhShYfYfYfYfShYhYhYhYhadeakiQSfSfSfUfShShShUfSfSfSfUfShShShUfSfSfSfcakQShShWfeQShShWeeQUhWfhUhShUfWjhQUfUfUfWfdQShShShWkhQUfUfUfchjQYhYhYhYhUfYfYfYeYhUfYhYhcifQYfYfYfYeQcffQYhYhYiYiYfcdhckjUfUfZfeYcciefhleiYhYcYhcfhYhcfhYhcifYhcfhYhcfhYhYcYh:C/2ecc713498db34495ee67e22ecf0f1000000');
+  });
+});
+
+test('convert hash to voxel data', function(t) {
+  t.plan(3);
+  var c = new Convert();
+  c.readImage(path.resolve('rabbit.png'), function(err, hash) {
+    var data = c.toVoxels(hash);
+    t.deepEqual(data.bounds, [[-3, -1, -1], [3, 12, 3]]);
+    t.equal(data.colors.length, 6);
+    t.equal(Object.keys(data.voxelData).length, 259);
+  });
+});
